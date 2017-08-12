44086a05841573d0487fe5b293b8bba2441191fb
hsshhsshhssh <346731187@qq.com>
2017-08-13 03:07:30 +0800

    fix

--- src/main/java/com/xqh/ad/controller/demo/DemoController.java
+++ src/main/java/com/xqh/ad/controller/demo/DemoController.java
@@ -1,18 +1,6 @@
 package com.xqh.ad.controller.demo;
 
-import com.xqh.ad.entity.vo.AdAppVO;
-import com.xqh.ad.tkmapper.entity.AdApp;
-import com.xqh.ad.tkmapper.mapper.AdAppMapper;
-import com.xqh.ad.utils.common.DozerUtils;
-import com.xqh.ad.utils.common.ExampleBuilder;
-import com.xqh.ad.utils.common.Search;
-import org.hssh.common.zkconf.Value;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tk.mybatis.mapper.entity.Example;
-
-import java.util.List;
 
 /**
  * Created by hssh on 2017/8/12.
@@ -20,24 +8,6 @@ import java.util.List;
 @RestController
 public class DemoController
 {
-    @Autowired
-    private AdAppMapper adAppMapper;
 
-    @Value(path = "/config/zkconf/ad/xqh_ad", key = "test")
-    private String test;
-
-    @GetMapping("get")
-    public String queryOne()
-    {
-        //Search search = new Search();
-        //search.put("name_eq", "1");
-        //Example example = new ExampleBuilder(AdApp.class).search(search).build();
-        //
-        //List<AdApp> adApps = adAppMapper.selectByExample(example);
-
-        //return DozerUtils.map(adApps.get(0), AdAppVO.class);
-
-        return test;
-    }
 
 }
