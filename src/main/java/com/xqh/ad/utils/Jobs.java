3e083017c99655986d3926346b346c15b2bd8d45
hsshhsshhssh <346731187@qq.com>
2018-04-22 14:21:36 +0800

    fix

--- src/main/java/com/xqh/ad/utils/Jobs.java
+++ src/main/java/com/xqh/ad/utils/Jobs.java
@@ -10,6 +10,7 @@ import com.xqh.ad.utils.common.Search;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.entity.Example;
 
